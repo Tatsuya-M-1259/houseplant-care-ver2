3f0efcfab7c97e5175bb54eb40786896e44ff745
Tatsuya-M-1259 <sstop1975@gmail.com>
2026-06-20 08:41:41 +0900

    Update service worker to version 41

--- sw.js
+++ sw.js
@@ -1,18 +1,4 @@
-// sw.js
-const CACHE_NAME = 'houseplant-care-v40'; 
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME));
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
-    );
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
-});
+const CACHE_NAME = 'houseplant-care-v41'; 
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME)));
+self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
